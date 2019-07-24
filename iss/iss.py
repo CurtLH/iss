a7387c77c57e5566146042e25d4bb8bb7af5a24e
CurtLH <CurtLHampton@gmail.com>
2019-07-23 21:58:50 -0700

    iterate

--- iss/iss.py
+++ iss/iss.py
@@ -1,7 +1,19 @@
 import requests
 
-def query():
+__all__ = [
+    "where",
+    "who"
+]
+
+def where():
 
     r = requests.get("http://api.open-notify.org/iss-now.json")
+
+    return r
+
+
+def who():
+
+    r = requests.get("http://api.open-notify.org/astros.json")
 
     return r
